43dcb050f31d8dfd37f74e043490b9acd43e3c79
John Woolley <woolley@berkeley.edu>
2021-05-31 17:03:10 -0400

    Added test to class file -> sbt run

--- src/main/scala/Cashflow.scala
+++ src/main/scala/Cashflow.scala
@@ -2,11 +2,19 @@ import java.time.LocalDate
 import scala.collection.immutable.Vector
 import cats.Monoid
 
-final case class Cashflow(date: LocalDate, amount: Float, currency: String)
-final case class CashflowSeq(cashflows: Vector[Cashflow])
-final case class CashflowPV(cashflow: Cashflow, refDate: LocalDate, refRate: Float)
-final case class CashflowPVSeq(cashflowPVs: Vector[CashflowPV])
-val CashflowSeqMonoid: Monoid[CashflowSeq] = new Monoid[CashflowSeq]{
-  def empty: CashflowSeq = CashflowSeq(Vector())
-  def combine(x: CashflowSeq, y: CashflowSeq): CashflowSeq = CashflowSeq(x.cashflows ++ y.cashflows)
+object Main extends App {
+    final case class Cashflow(date: LocalDate, amount: Float, currency: String)
+    final case class CashflowSeq(cashflows: Vector[Cashflow])
+    final case class CashflowPV(cashflow: Cashflow, refDate: LocalDate, refRate: Float)
+    final case class CashflowPVSeq(cashflowPVs: Vector[CashflowPV])
+    val CashflowSeqMonoid: Monoid[CashflowSeq] = new Monoid[CashflowSeq]{
+      def empty: CashflowSeq = CashflowSeq(Vector())
+      def combine(x: CashflowSeq, y: CashflowSeq): CashflowSeq = CashflowSeq(x.cashflows ++ y.cashflows)
+    }
+    val mycashflow = Cashflow(java.time.LocalDate.now, 0, "USD")
+    val myseq = CashflowSeq(Vector(mycashflow))
+    val myseq2 = CashflowSeq(Vector(mycashflow))
+    val combined = CashflowSeqMonoid.combine(myseq, myseq2)
+    println(combined)
 }
+
